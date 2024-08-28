be97f357ffa22e9708354e7789a9f455cfbd6550
Arun kumar <arunkumarjahanabad91@gmail.com>
2024-08-28 08:49:54 +0530

    3- payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,6 @@ import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
 
 
-
-
-
-
 dotenv.config();
 
 cloudinary.config({
@@ -26,7 +22,7 @@ cloudinary.config({
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit: "5mb"}));
 app.use(express.urlencoded({extended: true}));
 
 app.use(cookieParser());
